bbda4a5b324da7ae567b2a7c994860d225d4dc8e
Josef Antoni <>
2022-06-21 11:06:27 +0200

    Innitial commit, created xcode project, added PDF file with the task

---
+++ MySportPerformance/MySportPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySportPerformanceApp.swift
+//  MySportPerformance
+//
+//  Created by Pepca on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MySportPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySportPerformance/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dsadasdsa
+//
+//  Created by Pepca on 21.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
